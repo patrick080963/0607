e062f7a116287f46a0f84e349c5fc08f46d1e63c
Fuyuan Cheng <gloomcheng@gmail.com>
2024-05-31 16:46:09 +0800

    Add CustomCard component

---
+++ src/app/components/Card.jsx
@@ -0,0 +1,10 @@
+/**
+ * @author Fuyuan Cheng <gloomcheng@gmail.com>
+ */
+export default function CustomCard({item}) {
+    return (
+        <div className="border">
+            <div>{item.ScenicSpotName}</div>
+        </div>
+    )
+}
