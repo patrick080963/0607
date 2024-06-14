37dd871b0a820880933eb469646a85abebdddcdd
Bubu <patrick080963@gmail.com>
2024-06-14 17:06:05 +0800

    0614

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -1,9 +1,11 @@
 /**
  * @author Fuyuan Cheng <gloomcheng@gmail.com>
  */
+import Image from "next/image";
 export default function CustomCard({item}) {
     return (
         <div className="border">
+            <image src={item.Picture.PictureUrl1}alt={item.Picture.PictureDescription}/>
             <div>{item.ScenicSpotName}</div>
         </div>
     )
